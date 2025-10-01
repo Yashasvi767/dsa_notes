7c0af7105eb4411deaf7313727ebb1e781952c9a
yashu <113288809+Yashasvi767@users.noreply.github.com>
2025-10-01 19:58:22 +0530

    Create sumOfDigits2.cpp

---
+++ gfg/LogicBuilding/EasyProblems/SumOfDigits/sumOfDigits2.cpp
@@ -0,0 +1,18 @@
+# Recursive_method
+
+class Solution {
+  public:
+    int sumOfDigits(int n) {
+       
+        if (n==0)
+        {
+            return 0;
+        }
+        
+        else
+        {
+            return (n%10) + sumOfDigits(n/10);
+        }
+    }
+    
+};
