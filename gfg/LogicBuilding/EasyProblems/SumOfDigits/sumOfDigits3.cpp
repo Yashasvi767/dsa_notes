1883ab6830f087ad4b06fe7741841e7b0ae25668
yashu <113288809+Yashasvi767@users.noreply.github.com>
2025-10-01 20:33:36 +0530

    Update sumOfDigits3.cpp

--- gfg/LogicBuilding/EasyProblems/SumOfDigits/sumOfDigits3.cpp
+++ gfg/LogicBuilding/EasyProblems/SumOfDigits/sumOfDigits3.cpp
@@ -1,14 +1,14 @@
 class Solution {
   public:
     int sumOfDigits(int n) {
-        int sum=0;
-        
-        while(n!=0){
-            int last = n%10;
-            sum+=last;
-            n/=10;
-        }
-        return sum;
+       string s = to_string(n);
+       int sum=0;
+       
+       for (char ch : s)
+       {
+          sum+=ch-'0' ;
+       }
+       
+       return sum;
     }
-    
 };
