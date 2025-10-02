b606b6a22e24b2321bdb533d4335805c2b787854
yashu <113288809+Yashasvi767@users.noreply.github.com>
2025-10-02 14:28:52 +0530

    Update PrimeTesting.cpp

--- gfg/LogicBuilding/EasyProblems/PrimeTesting/PrimeTesting.cpp
+++ gfg/LogicBuilding/EasyProblems/PrimeTesting/PrimeTesting.cpp
@@ -4,7 +4,7 @@ class Solution {
         
         if(n<=1)
             return false;
-        for(int i=2; i<n; i++){
+        for(int i=2; i<=sqrt(n); i++){
             if (n%i==0)
             return false;
             
