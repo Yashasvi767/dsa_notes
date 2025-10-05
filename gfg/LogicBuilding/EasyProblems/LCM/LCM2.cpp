8c290591e2d9f276d5e9363de0f4373b27cb3ac0
yashu <113288809+Yashasvi767@users.noreply.github.com>
2025-10-05 15:21:50 +0530

    Create LCM2.cpp

---
+++ gfg/LogicBuilding/EasyProblems/LCM/LCM2.cpp
@@ -0,0 +1,15 @@
+// GCD LCM formula
+
+class Solution {
+  public:
+    int lcm(int a, int b) {
+         
+        int gcd(int a, int b) {
+           return (b == 0) ? a : gcd(b, a % b);
+}
+
+        int lcm(int a, int b) {
+            return (a / gcd(a, b)) * b;
+}
+    }
+};
